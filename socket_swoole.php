9be04d21603a64a838e96725d97406f0b89256fe
haha03942008 <648588267@qq.com>
2017-08-23 17:59:11 +0800

    修改并发bug

--- socket_swoole.php
+++ socket_swoole.php
@@ -3,18 +3,18 @@
     $redis->connect('127.0.0.1', 6379);
     $ser = new swoole_websocket_server("172.17.46.31",8080);
     $ser->on('open', function($ser, $request) use($redis) {
-        $map[$request->fd] = (string)$request->fd;
-        $redis->hmset('uid', $map);
-        $ser->push($request->fd, 'connect success');
+	$map[$request->fd] = (string)$request->fd;
+	$redis->hmset('uid', $map);
+    	$ser->push($request->fd, 'connect success');
     });
     $ser->on('message', function($ser, $a) use($redis) {
-        $map = [];
-        $map = $redis->hgetall('uid');
-        foreach ($map as $v) {
-            $ser->push($v, $a->data);
-        }
+	$map = [];
+	$map = $redis->hgetall('uid');
+	foreach ($map as $v) {
+	    $ser->push($v, $a->data);
+	}
     });
     $ser->on('close', function($ser, $fd) use($redis) {
-        $redis->hdel('uid', $fd);
+	$redis->hdel('uid', $fd);
     });
     $ser->start();
